248376c8ac6fd418651b0efd7c66941571fe500e
Kamil Parczewski <kamil.parczewski@o2.pl>
2019-04-27 12:09:26 +0200

    Add input with label

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      BMI: '90',
+    };
+  }
+
+  onChangeBMI = (value) => {
+    this.setState({
+      BMI: value,
+    })
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello world!</Text>
+        <Text style={{fontSize: 30}}>{this.state.BMI}</Text>
+
+        <View style={styles.inputWrapper}>
+          <Text style={styles.label}>BMI</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={this.onChangeBMI}
+            value={this.state.BMI}
+          />
+        </View>
+
       </View>
     );
   }
@@ -17,5 +41,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 20,
   },
+  input: {
+    width: '100%',
+    borderBottomWidth: 3,
+    borderColor: '#0080FF',
+    height: 40,
+    borderRadius: 3,
+    fontWeight: 'bold',
+  },
+  inputWrapper: {
+    width: '100%',
+  },
+  label: {
+    color: '#0080FF',
+    fontWeight: 'bold',
+  }
 });
