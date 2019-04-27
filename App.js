919e512005396b1d8121f3f064a402c24bd3005e
Kamil Parczewski <kamil.parczewski@o2.pl>
2019-04-27 14:04:31 +0200

    02-components_flat-list

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
   StyleSheet,
-  Text,
   View,
-  TextInput,
-  TouchableOpacity,
+  FlatList,
+  Text,
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // https://expo.github.io/vector-icons/
 
 import Input from './components/Input'
 import Button from './components/Button'
@@ -16,6 +16,18 @@ export default class App extends React.Component {
 
     this.state = {
       BMI: '90',
+      weights: [
+        {
+          id: 1,
+          value: '90',
+          date: '12.08.2017'
+        },
+        {
+          id: 2,
+          value: '86',
+          date: '12.08.2016'
+        }
+      ],
     };
   }
 
@@ -29,11 +41,28 @@ export default class App extends React.Component {
     console.log(this.state.BMI);
   };
 
+  keyExtractor = (item) => {
+    return `${item.id}`;
+  };
+
+  renderItem = ({item}) => {
+    return (
+      <View style={styles.weightRow}>
+        <Text style={styles.weightValue}>{item.value}</Text>
+        <Text style={styles.weightDate}>{item.date}</Text>
+        <View style={styles.iconWrapper}>
+          <Ionicons name="md-create" size={16} color="gray" />
+        </View>
+        <View style={[styles.iconWrapper, styles.closeIcon]}>
+          <Ionicons name="md-close" size={16} color="gray" />
+        </View>
+      </View>
+    )
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{fontSize: 30}}>{this.state.BMI}</Text>
-
         <Input
           onChangeText={this.onChangeBMI}
           label="BMI"
@@ -42,11 +71,19 @@ export default class App extends React.Component {
           placeholder="Placeholder"
         />
 
+        <View style={styles.listContainer}>
+          <FlatList
+            data={this.state.weights}
+            renderItem={this.renderItem}
+            keyExtractor={this.keyExtractor}
+          />
+
+        </View>
+
         <Button
           onPress={this.onPressButton}
           text="ADD WEIGHT"
         />
-
       </View>
     );
   }
@@ -57,7 +94,39 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     marginHorizontal: 20,
+    marginVertical: 40
   },
+  listContainer: {
+    flex: 1,
+    width: '100%'
+  },
+  weightRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: 50,
+  },
+  weightValue: {
+    fontWeight: 'bold',
+  },
+  weightDate: {
+    fontWeight: 'bold',
+    flex: 1,
+    textAlign: 'center'
+  },
+  iconWrapper: {
+    height: 24,
+    width: 24,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: 'gray',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  closeIcon: {
+    marginLeft: 15,
+  }
 });
