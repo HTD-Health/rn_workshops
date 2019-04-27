176a24f8c3032b1c9d97dfbc623fa8f930e0b561
Lukasz Debowski <lukasz.debowski@htdevelopers.com>
2019-04-27 20:03:18 +0200

    tests setup

---
+++ screens/AddWeightScreen/index.test.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { shallow } from "enzyme"
+import WeightScreen from "./AddWeightScreen";
+
+describe("bla", () => {
+  it("renders", () => {
+    const wrapper = shallow(<WeightScreen />)
+  })
+})
