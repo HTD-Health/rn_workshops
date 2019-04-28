fd587f8cf8934b739aa12bc5b037ff663cda93ce
dwojcik96 <dwojcik@classpass.com>
2019-04-28 15:28:22 +0200

    Fetch data & basic views

--- screens/ProductsScreen/ProductsScreen.js
+++ screens/ProductsScreen/ProductsScreen.js
@@ -1,5 +1,41 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, FlatList } from "react-native";
 
-const ProductScreen = () => <Text>Products</Text>;
-export default ProductScreen;
+export default class ProductsScreen extends React.Component {
+  state = {
+    loading: false,
+    products: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    fetch("http://193.70.34.240/rnapi/api/v1/products")
+      .then(response => response.json())
+      .then(response =>
+        this.setState({
+          products: response,
+          loading: false
+        })
+      );
+  }
+
+  render() {
+    const { loading, products } = this.state;
+
+    if (loading) {
+      return <Text>Loading</Text>;
+    }
+
+    return (
+      <View>
+        <FlatList
+          data={products}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
+    );
+  }
+}
