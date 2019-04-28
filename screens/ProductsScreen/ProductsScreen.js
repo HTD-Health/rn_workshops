d542c743894dac95bad4247144b92cf0a3ab3277
dwojcik96 <dwojcik@classpass.com>
2019-04-28 15:57:59 +0200

    Add styles & new component

--- screens/ProductsScreen/ProductsScreen.js
+++ screens/ProductsScreen/ProductsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 
 export default class ProductsScreen extends React.Component {
   state = {
@@ -29,13 +29,50 @@ export default class ProductsScreen extends React.Component {
     }
 
     return (
-      <View>
+      <View style={productsStyles.container}>
         <FlatList
           data={products}
-          renderItem={({ item }) => <Text>{item.name}</Text>}
+          renderItem={({ item }) => <ProductItem item={item} />}
           keyExtractor={item => item.id.toString()}
         />
       </View>
     );
   }
 }
+
+const productsStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "#D7E8FF",
+    flex: 1
+  }
+});
+
+const ProductItem = ({ item }) => (
+  <View style={productStyles.container}>
+    <View style={productStyles.textContainer}>
+      <Text style={productStyles.name}>{item.name}</Text>
+      <Text numberOfLines={3}>{item.description}</Text>
+    </View>
+    <Image style={productStyles.picture} source={{ uri: item.photo }} />
+  </View>
+);
+
+const productStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    borderColor: "gray",
+    flexDirection: "row",
+    padding: 10,
+    borderWidth: 0.2,
+    height: 90
+  },
+  textContainer: {
+    flex: 2
+  },
+  name: {
+    fontWeight: "bold"
+  },
+  picture: {
+    flex: 1
+  }
+});
